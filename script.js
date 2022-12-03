35e026d017c18b13d297fa2be3b3e0a0dc747144
keller-johnson <keller.s.johnson@gmail.com>
2022-12-03 14:32:15 -0800

    archiving solution

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ const commonItems = rucksacks.map(r => {
     //using .length and / to equally split the string in two segments -- .slice first param is starting point and second param is where to end
     const firstCompartment = r.slice(0, r.length / 2).split('');
     const secondCompartment = r.slice(r.length / 2).split('');
-    //running a function to calculate points IF a char from first part matches in second part
+    //running a function to calculate points depending on which char from first part matches in second part
     return getCharPoints(firstCompartment.filter(e=> secondCompartment.includes(e))[0])
 })
-
+//function uses charCodeAt method with an offset to return the proper value for lower case and upper case
 function getCharPoints(char) {
     if (char === char.toUpperCase()) {
         return Number(char.charCodeAt(0) - 38)
@@ -26,6 +26,7 @@ function getCharPoints(char) {
     }
 }
 
+//now taking the data and reducing it into the final number
 const firstPuzzleSolution = commonItems.reduce((acc, curr) => acc + curr)
 
 
